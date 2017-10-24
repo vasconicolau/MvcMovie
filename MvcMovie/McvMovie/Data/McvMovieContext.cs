7e4af4ef9da51327b7b2eed20850bab4f58776f9
Vasco Nicolau <150221015@estudantes.ips.pt>
2017-10-24 09:08:33 +0100

    Adding a model to an ASP.NET Core MVC app

---
+++ MvcMovie/McvMovie/Data/McvMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace McvMovie.Models
+{
+    public class McvMovieContext : DbContext
+    {
+        public McvMovieContext (DbContextOptions<McvMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
--- MvcMovie/McvMovie/Startup.cs
+++ MvcMovie/McvMovie/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using McvMovie.Models;
 
 namespace McvMovie
 {
@@ -22,6 +24,9 @@ namespace McvMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<McvMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("McvMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
