14e9f5d1c88813bdbb79cd84bdaefa8c007deb8f
Vasco Nicolau <150221015@estudantes.ips.pt>
2017-10-24 10:51:35 +0100

    Adding a New Field

--- MvcMovie/McvMovie/Controllers/MoviesController.cs
+++ MvcMovie/McvMovie/Controllers/MoviesController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using McvMovie.Models;
 using MvcMovie.Models;
 
 namespace McvMovie.Controllers
@@ -77,7 +76,7 @@ namespace McvMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
--- MvcMovie/McvMovie/Migrations/20171024094730_Rating.Designer.cs
+++ MvcMovie/McvMovie/Migrations/20171024094730_Rating.Designer.cs
@@ -8,12 +8,13 @@ using Microsoft.EntityFrameworkCore.Storage.Internal;
 using MvcMovie.Models;
 using System;
 
-namespace MvcMovie.Migrations
+namespace McvMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20171024094730_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
